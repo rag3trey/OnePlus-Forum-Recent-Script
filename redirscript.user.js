052de650525c7e760f10408fb603e63d84bc366f
rag3trey <rag3trey@users.noreply.github.com>
2017-06-22 15:41:25 -0500

    Update redirscript.user.js

--- redirscript.user.js
+++ redirscript.user.js
@@ -4,13 +4,10 @@
 // @version      0.1
 // @description  Redirect OnePlus Forums to Recents instead of Recommended
 // @author       rag3trey
-// @match        *forums.oneplus.net/*
+// @match        *forums.oneplus.net/
 // @grant        none
 // ==/UserScript==
 
-document.body.addEventListener('mousedown', function(e){
-    var targ = e.target || e.srcElement;
-    if ( targ && targ.href && targ.href.match(/https?:\/\/forums.oneplus.net/) ) {
-        targ.href = targ.href.replace(/https?:\/\/forums.oneplus\.net/, 'http://forums.oneplus.net/?order=recent');
-    }
-});
+var groupname = "?order=recent"; //insert your groupname between these brackets
+
+location.href = location.protocol + "//forums.oneplus.net" + groupname;
