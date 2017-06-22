fe130b61591be6d38a03419dcdd3b7b2cd4d2ea1
rag3trey <rag3trey@users.noreply.github.com>
2017-06-22 14:59:23 -0500

    Create redirscript.user.js

---
+++ redirscript.user.js
@@ -0,0 +1,19 @@
+
+// ==UserScript==
+// @name         Recommended -> Recents
+// @version      0.1
+// @description  Redirect OnePlus Forums to Recents instead of Recommended
+// @author       rag3trey
+// @match        *forums.oneplus.net/*
+// @grant        none
+// ==/UserScript==
+
+$(document).ready(function () {
+	function change() {
+		$(".homeLabel:contains(Home)")window.location = "https://forums.oneplus.net/?order=recent";
+	}
+	$(document).ajaxStop(function () {
+		change();
+	});
+	change();
+});
